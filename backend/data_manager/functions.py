a76aecd68f197b6b8ed6b9a4561f0b9c3602f74f
Szuum02 <s.urbanski2002@gmail.com>
2024-05-19 00:19:00 +0200

    1d all numeric stats

--- backend/data_manager/functions.py
+++ backend/data_manager/functions.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-from scipy import stats
 from backend.settings import FILES_URL
 from data_manager.available_stats import numpy_functions, scipy_functions
 
@@ -11,17 +10,17 @@ def get_file_url(destination: str) -> str:
         return str(os.path.join(directory_path, os.listdir(directory_path)[0]))
     return ""
 
+
+def convert_data(value):
+    try:
+        return float(value)
+    except ValueError:
+        return np.nan
+
+
 def calculate_stats(function_name: str, data):
     if function_name in numpy_functions.keys():
         return numpy_functions.get(function_name)(data)
     if function_name in scipy_functions.keys():
         return scipy_functions.get(function_name)(data)
-    # if function_name == "mode":
-    #     return stats.mode(data).mode[0]
-    # if function_name == "precentile":
-    #     return np.percentile(data, [25, 50, 75])
-    # if function_name == "isnan":
-    #     return np.isnan(data).sum()
     # TODO: error handle
-
-
