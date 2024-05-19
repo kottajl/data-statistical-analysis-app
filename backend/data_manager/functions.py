f41a906d9ecb5dc263d24e88f62ebabf16855d9d
AdMrzyglod <admrzyglod@student.agh.edu.pl>
2024-05-19 22:34:50 +0200

    Handling missing and outlier values

--- backend/data_manager/functions.py
+++ backend/data_manager/functions.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 from scipy import stats
 from backend.settings import FILES_URL
 from data_manager.available_stats import numpy_functions, scipy_functions
@@ -9,7 +10,19 @@ def get_file_url(destination: str) -> str:
     directory_path = str(os.path.join(FILES_URL, destination))
     if os.path.exists(directory_path) and len(os.listdir(directory_path)) == 1:
         return str(os.path.join(directory_path, os.listdir(directory_path)[0]))
-    return ""
+    raise Exception("File not found.")
+
+
+def load_to_dataframe(destination: str) -> pd.DataFrame:
+    file_url = get_file_url(destination)
+    df = pd.read_csv(file_url, sep=';', decimal=",")
+    return df
+
+
+def save_from_dataframe(destination: str, df: pd.DataFrame) -> None:
+    file_url = get_file_url(destination)
+    df.to_csv(file_url, index=False, sep=';', decimal=",")
+
 
 def calculate_stats(function_name: str, data):
     if function_name in numpy_functions.keys():
@@ -23,5 +36,3 @@ def calculate_stats(function_name: str, data):
     # if function_name == "isnan":
     #     return np.isnan(data).sum()
     # TODO: error handle
-
-
--- backend/data_manager/views.py
+++ backend/data_manager/views.py
@@ -1,13 +1,15 @@
 import os
 
 import numpy as np
+import uuid
 from django.http import FileResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from backend.settings import FILES_URL
-import uuid
-from data_manager.functions import get_file_url, calculate_stats
+from data_manager.functions import get_file_url, calculate_stats, load_to_dataframe, save_from_dataframe
+
+from data_manager.missing_values_functions import complete_missing_values,replace_outliers_to_nan
 
 
 @api_view(["POST"])
@@ -35,11 +37,35 @@ def data_import(request):
 @api_view(["POST"])
 def data_export(request):
     if request.method == "POST":
-        if "destination" in request.data and isinstance(request.data["destination"], str) and get_file_url(
-                request.data["destination"]) != "":
-            return FileResponse(open(get_file_url(request.data["destination"]), 'rb'), content_type="text/csv",
-                                as_attachment=True)
-        return Response(data=dict(detail="File does not exist"), status=status.HTTP_400_BAD_REQUEST)
+        try:
+            if "destination" in request.data and isinstance(request.data["destination"], str):
+                file_url = get_file_url(request.data["destination"])
+                return FileResponse(open(file_url, 'rb'), content_type="text/csv",
+                                    as_attachment=True)
+            raise Exception("Invalid data")
+        except Exception as e:
+            return Response(data=dict(detail=str(e)), status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def fill_missing_values(request):
+    if request.method == "POST":
+        try:
+            if "destination" in request.data and isinstance(request.data["destination"],
+                                                            str) and "variable" in request.data and "method" in request.data and "outliers" in request.data:
+                destination = request.data["destination"]
+                variable, method, outliers = request.data["variable"], request.data["method"], request.data["outliers"]
+                df = load_to_dataframe(destination)
+                if outliers:
+                    replace_outliers_to_nan(df, variable)
+                complete_missing_values(df, variable, method, request.data.get("constant"))
+                save_from_dataframe(destination, df)
+
+                return Response(data=dict(detail="Variable updated"), status=status.HTTP_200_OK)
+            raise Exception("Invalid data")
+        except Exception as e:
+            return Response(data=dict(detail=str(e)), status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["GET"])
 def stats_1d(request):
