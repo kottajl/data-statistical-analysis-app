f519cc3c4ce4fdfa96056a03da499f4e27deca0d
AdMrzyglod <admrzyglod@student.agh.edu.pl>
2024-05-21 21:19:24 +0200

    1d visualizations updated

--- backend/data_visualization/plots_functions.py
+++ backend/data_visualization/plots_functions.py
@@ -1,94 +1,134 @@
 import io
 
+import numpy as np
 import pandas as pd
 import matplotlib
 
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import distinctipy
 
 
-def draw_plot_1d(series: pd.Series, plot_type: str, ID_exists: bool):
+def draw_plot_1d(df: pd.DataFrame, plot_type: str):
     match plot_type:
         case "scatter":
-            return plot_scatter(series, ID_exists)
+            return plot_scatter(df)
         case "linear":
-            return plot_linear(series, ID_exists)
+            return plot_linear(df)
         case "hist":
-            return plot_histogram(series)
+            return plot_histogram(df)
         case "bar":
-            return plot_bar(series, ID_exists)
+            return plot_bar(df)
         case "pie_chart":
-            return plot_pie_chart(series, ID_exists)
+            return plot_pie_chart(df)
         case "boxplot":
-            return plot_boxplot(series)
+            return plot_boxplot(df)
         case _:
             raise ValueError(f"Unrecognized plot type: {plot_type}")
 
 
-def plot_scatter(series: pd.Series, ID_exists: bool):
-    if not ID_exists:
+def plot_scatter(df: pd.DataFrame):
+    if "ID" not in df.columns:
         raise ValueError("ID not sent.")
-    plot = plt.figure(figsize=(10, 8))
-    plt.scatter(series.index, series.values, label='Punkty')
+    plot = plt.figure(figsize=(12, 8))
+    plt.subplots_adjust(right=0.75)
+    for column in df.columns:
+        if column == "ID":
+            continue
+        plt.scatter(df["ID"], df[column], label=column)
     plt.title("Wykres punktowy")
     plt.xlabel("ID")
     plt.ylabel("Wartości")
-    plt.legend()
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.75))
     plt.grid(True)
     return get_plot_file(plot)
 
 
-def plot_linear(series: pd.Series, ID_exists: bool):
-    if not ID_exists:
+def plot_linear(df: pd.DataFrame):
+    if "ID" not in df.columns:
         raise ValueError("ID not sent.")
-    plot = plt.figure(figsize=(10, 8))
-    plt.plot(series.index, series.values, label='Wykres')
+    plot = plt.figure(figsize=(12, 8))
+    plt.subplots_adjust(right=0.75)
+    for column in df.columns:
+        if column == "ID":
+            continue
+        plt.plot(df["ID"], df[column], label=column)
     plt.title("Wykres liniowy")
     plt.xlabel("ID")
     plt.ylabel("Wartości")
-    plt.legend()
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.75))
     plt.grid(True)
     return get_plot_file(plot)
 
 
-def plot_histogram(series: pd.Series):
-    plot = plt.figure(figsize=(10, 8))
-    plt.hist(series.values, bins=25, edgecolor='black')
+def plot_histogram(df: pd.DataFrame):
+    plot = plt.figure(figsize=(12, 8))
+    plt.subplots_adjust(right=0.75)
+    columns = [col for col in df.columns if col != 'ID']
+    plt.hist([df[column] for column in columns], 30, label=columns)
     plt.title("Histogram")
-    plt.xlabel("Wartość")
-    plt.ylabel("Częstość")
+    plt.xlabel("Wartości")
+    plt.ylabel("Częstości")
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.75))
     plt.grid(True)
     return get_plot_file(plot)
 
 
-def plot_bar(series: pd.Series, ID_exists: bool):
-    if not ID_exists:
+def plot_bar(df: pd.DataFrame):
+    if "ID" not in df.columns:
         raise ValueError("ID not sent.")
-    plot = plt.figure(figsize=(10, 8))
-    plt.bar(series.index, series.values, color='skyblue')
+    plot = plt.figure(figsize=(14, 8))
+    plt.subplots_adjust(right=0.75)
+    n_bars = len(df.columns) - 1
+    bar_width = 0.8 / n_bars
+    single_width = 1
+    colors = distinctipy.get_colors(n_bars)
+
+    X_values = df["ID"] if df["ID"].dtypes != "object" else np.arange(len(df["ID"]))
+    for i, column in enumerate([value for value in df.columns if value != "ID"]):
+        x_offset = (i - n_bars / 2) * bar_width + bar_width / 2
+
+        j = 0
+        for x, y in zip(X_values, df[column]):
+            if j == 0:
+                plt.bar(x + x_offset, y, width=bar_width * single_width, color=colors[i % len(colors)], label=column)
+            else:
+                plt.bar(x + x_offset, y, width=bar_width * single_width, color=colors[i % len(colors)])
+            j += 1
+
+    if df["ID"].dtypes == "object":
+        plt.xticks(X_values, df["ID"])
     plt.title("Wykres kolumnowy")
     plt.xlabel("ID")
     plt.ylabel("Wartości")
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.75))
     plt.grid(axis="y")
     return get_plot_file(plot)
 
 
-def plot_pie_chart(series: pd.Series, ID_exists: bool):
-    if not ID_exists:
-        raise ValueError("ID not sent.")
-    plot = plt.figure(figsize=(10, 8))
-    plt.pie(series.values, labels=series.index, autopct='%1.1f%%', startangle=90)
-    plt.title("Wykres kołowy")
-    plt.axis("equal")
-    plt.legend()
-    return get_plot_file(plot)
+def plot_pie_chart(df: pd.DataFrame):
+    column_len = len(df.columns) - 1
+    fig, axs = plt.subplots(len(df.columns) - 1, figsize=(12, 5 * column_len))
+    plt.subplots_adjust(right=0.75)
+    for i, column in enumerate([value for value in df.columns if value != "ID"]):
+        ax = axs[i]
+        value_count = df[column].value_counts()
+        ax.pie(value_count, labels=value_count.index, colors=distinctipy.get_colors(len(value_count)),
+               autopct='%1.1f%%', startangle=90)
+        ax.set_title(f"Wykres kołowy dla {column}")
+        ax.axis("equal")
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        ax.grid(True)
+    return get_plot_file(fig)
 
 
-def plot_boxplot(series: pd.Series):
-    plot = plt.figure(figsize=(10, 8))
-    plt.boxplot(series.values)
+def plot_boxplot(df: pd.DataFrame):
+    plot = plt.figure(figsize=(12, 8))
+    plt.subplots_adjust(right=0.75)
+    columns = [col for col in df.columns if col != 'ID']
+    plt.boxplot([df[column] for column in columns], labels=columns)
     plt.title("Wykres pudełkowy")
-    plt.ylabel("Wartość")
+    plt.ylabel("Wartości")
     plt.grid(True)
     return get_plot_file(plot)
 
--- backend/data_visualization/views.py
+++ backend/data_visualization/views.py
@@ -14,18 +14,21 @@ from data_visualization.plots_functions import draw_plot_1d
 def create_plot(request):
     if request.method == "POST":
         try:
-            if "data[]" in request.data and "plot_type" in request.data and "data_type" in request.data:
+            if "data[]" in request.data and "data_types[]" in request.data and "variable_names[]" in request.data and "plot_type" in request.data:
                 string_data, ID = request.data.getlist("data[]"), request.data.getlist("ID[]")
-                plot_type, data_type, ID_type = request.data["plot_type"], request.data["data_type"], request.data.get(
-                    "ID_type")
-                plot_data = [convert_data(value) for value in string_data] if data_type == "numerical" else string_data
-                plot_ID = None
+                data_types, variable_names = request.data.getlist("data_types[]"), request.data.getlist("variable_names[]")
+                plot_type, ID_type = request.data["plot_type"], request.data.get("ID_type")
+                plot_data = [[convert_data(value) for value in data_series] if type == "numerical" else data_series for
+                             type, data_series in zip(data_types, string_data)]
+                df = pd.DataFrame(data=np.transpose(plot_data), columns=variable_names)
                 if len(ID) > 0 and ID_type is not None:
-                    plot_ID = [convert_data(value) for value in ID] if ID_type == "numerical" else ID
-                series = pd.Series(plot_data, index=plot_ID)
-                buffer = draw_plot_1d(series, plot_type, len(ID) > 0)
+                    df["ID"] = [convert_data(value) for value in ID] if ID_type == "numerical" else ID
+                buffer = draw_plot_1d(df, plot_type)
                 return FileResponse(buffer, as_attachment=True, filename="plot-" + str(uuid.uuid4()) + ".png",
                                     status=status.HTTP_200_OK)
             raise Exception("Invalid data")
         except Exception as e:
             return Response(data=dict(detail=str(e)), status=status.HTTP_400_BAD_REQUEST)
+
+
+
