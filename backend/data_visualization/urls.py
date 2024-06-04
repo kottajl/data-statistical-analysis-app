af6e9ed0d0f5aa3fe1728e53251a8aa6553909db
AdMrzyglod <admrzyglod@student.agh.edu.pl>
2024-06-04 17:59:39 +0200

    urls updated

--- backend/data_visualization/urls.py
+++ backend/data_visualization/urls.py
@@ -3,6 +3,6 @@ import data_visualization.views as views
 
 
 urlpatterns = [
-    path('1d/', views.create_plot_2d),
+    path('1d/', views.create_plot_1d),
     path('2d/', views.create_plot_2d),
 ]
